7d986b255b3e2caa3cd1dd288030c0164e03ba31
Ryan P. Brewster <RyanPBrewster@gmail.com>
2023-04-07 20:15:19 -0400

    sst utils

--- src/bin/create.rs
+++ src/bin/create.rs
@@ -1,8 +1,13 @@
-use std::{path::PathBuf, io::{BufReader, BufRead}, fs::File};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    path::PathBuf,
+};
 
 use aws_config::meta::region::RegionProviderChain;
 use aws_sdk_s3::{config::Region, primitives::ByteStream, Client};
 use clap::Parser;
+use tracing::log::info;
 
 #[derive(Debug, Parser)]
 struct Opt {
@@ -19,6 +24,12 @@ struct Opt {
 
     #[arg(long)]
     input: PathBuf,
+
+    #[arg(long)]
+    output: PathBuf,
+
+    #[arg(long, default_value_t = false)]
+    skip_s3: bool,
 }
 
 #[tokio::main]
@@ -30,28 +41,51 @@ async fn main() -> anyhow::Result<()> {
         bucket,
         prefix,
         input,
+        output,
+        skip_s3,
     } = Opt::parse();
 
+    let mut db_opts = rocksdb::Options::default();
+    db_opts.create_if_missing(true);
+    db_opts.set_compression_type(rocksdb::DBCompressionType::Zstd);
+    let mut db = rocksdb::DB::open(&db_opts, output)?;
+
     let region_provider = RegionProviderChain::first_try(Region::new(region));
-
-    println!();
-
     let shared_config = aws_config::from_env().region(region_provider).load().await;
     let client = Client::new(&shared_config);
 
+    info!("opening {:?}", input);
     let fin = BufReader::new(File::open(input)?);
     for line in fin.lines() {
         let line = line?;
+        let parsed: serde_json::Value = serde_json::from_str(&line)?;
         let digest = ring::digest::digest(&ring::digest::SHA256, line.as_bytes());
-        let key = format!("{}/{}", prefix, hex::encode(digest.as_ref()));
-        client
-            .put_object()
-            .bucket(&bucket)
-            .key(&key)
-            .body(ByteStream::from(line.into_bytes()))
-            .send()
-            .await
+        let name = hex::encode(digest.as_ref());
+
+        let primary_key = parsed
+            .get("properties")
+            .unwrap()
+            .get("BLKLOT")
+            .unwrap()
+            .as_str()
             .unwrap();
+        let write_opts = {
+            let mut opts = rocksdb::WriteOptions::default();
+            opts.disable_wal(true);
+            opts
+        };
+        db.put_opt(primary_key, digest, &write_opts)?;
+
+        if skip_s3 {
+            client
+                .put_object()
+                .bucket(&bucket)
+                .key(&format!("{}/{}", prefix, name))
+                .body(ByteStream::from(line.into_bytes()))
+                .send()
+                .await
+                .unwrap();
+        }
     }
 
     Ok(())
---
+++ src/bin/sst.rs
@@ -0,0 +1,103 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    path::PathBuf,
+};
+
+use clap::{Parser, Subcommand};
+use rocksdb::SstFileWriter;
+use tracing::log::info;
+
+#[derive(Debug, Parser)]
+struct Opt {
+    #[command(subcommand)]
+    cmd: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    #[command(name = "cat")]
+    Cat {
+        #[arg(long)]
+        input: Vec<PathBuf>,
+    },
+    #[command(name = "make-sst")]
+    MakeSst {
+        #[arg(long)]
+        input: PathBuf,
+
+        #[arg(long)]
+        output: PathBuf,
+    },
+
+    #[command(name = "merge")]
+    Merge {
+        #[arg(long)]
+        input: Vec<PathBuf>,
+
+        #[arg(long)]
+        output: PathBuf,
+    },
+}
+
+fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let opt = Opt::parse();
+    match opt.cmd {
+        Command::Cat { input} => {
+            for i in input {
+                cat_db(i)?;
+            }
+        },
+        Command::MakeSst { input, output } => make_sst(input, output)?,
+        Command::Merge { input, output } => merge_ssts(input, output)?,
+    };
+    Ok(())
+}
+
+fn cat_db(input: PathBuf) -> anyhow::Result<()> {
+    let db = rocksdb::DB::open(&rocksdb::Options::default(), input)?;
+    for item in db.iterator(rocksdb::IteratorMode::Start) {
+        let (k, v) = item?;
+        info!("{}={:?}", std::str::from_utf8(&k)?, v);
+    }
+    Ok(())
+}
+
+
+fn make_sst(input: PathBuf, output: PathBuf) -> anyhow::Result<()> {
+    let mut db_opts = rocksdb::Options::default();
+    db_opts.create_if_missing(true);
+    db_opts.set_compression_type(rocksdb::DBCompressionType::Zstd);
+    let mut db = rocksdb::SstFileWriter::create(&db_opts);
+    db.open(output)?;
+
+    info!("opening {:?}", input);
+    let fin = BufReader::new(File::open(input)?);
+    for line in fin.lines() {
+        let line = line?;
+        let parsed: serde_json::Value = serde_json::from_str(&line)?;
+        let digest = ring::digest::digest(&ring::digest::SHA256, line.as_bytes());
+        let primary_key = parsed
+            .get("properties")
+            .unwrap()
+            .get("BLKLOT")
+            .unwrap()
+            .as_str()
+            .unwrap();
+        db.put(primary_key, digest)?;
+    }
+    db.finish()?;
+    Ok(())
+}
+
+
+fn merge_ssts(inputs: Vec<PathBuf>, output: PathBuf) -> anyhow::Result<()> {
+    let mut db_opts = rocksdb::Options::default();
+    db_opts.create_if_missing(true);
+    db_opts.set_compression_type(rocksdb::DBCompressionType::Zstd);
+    let db = rocksdb::DB::open(&db_opts, output)?;
+    db.ingest_external_file(inputs)?;
+    Ok(())
+}
